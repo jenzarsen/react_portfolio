e41349481f3f83842203106fc524cfd20fa938c3
jenzarsen <jenzarsen.alabado@gmail.com>
2024-12-17 15:02:10 +0800

    Fix landing page link

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -14,6 +14,7 @@ function App() {
       >
         <div style={{ flex: 1 }}>
           <Routes>
+            <Route path="/react_portfolio" element={<Home />} />
             <Route path="/" element={<Home />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/experience" element={<Experience />} />
